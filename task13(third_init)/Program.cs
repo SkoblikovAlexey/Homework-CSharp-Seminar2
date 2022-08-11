64e5556005c97e274bfd198976666c8887faa8ea
Alexey Skoblikov <happycow32@yandex.ru>
2022-08-11 16:02:01 +0300

    Task 13

---
+++ task13(third_init)/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру 
+// заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+while (true)
+{
+    Console.WriteLine("Введите число.");
+    int number = Convert.ToInt32(Console.ReadLine());
+    string str = Convert.ToString(number);
+    if (str.Length > 2) {
+Console.WriteLine(str[2]);
+        }
+        else {
+            Console.WriteLine("У введенного числа нет третьей цифры.");
+        }
+    Console.WriteLine("Для завершения введите end, для продолжения - любой символ");
+    string a = Console.ReadLine();
+    if (a == "end")
+    {
+        break;
+    }
+}
