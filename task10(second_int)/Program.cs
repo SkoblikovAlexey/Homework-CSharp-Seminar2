1e206bace88a8425ce69770e322e2a4f873b8ea3
Alexey Skoblikov <happycow32@yandex.ru>
2022-08-11 04:12:05 +0300

    Task10

---
+++ task10(second_int)/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+//  и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+bool calculation = true;
+while (calculation)
+{
+    Console.WriteLine("Введите трёхзначное число.");
+    int number = Convert.ToInt32(Console.ReadLine());
+    int temp = number / 10;
+    int result = temp % 10;
+    Console.WriteLine(result);
+    Console.WriteLine("Для завершения введите end, для продолжения - любой символ");
+    string a = Console.ReadLine();
+    if (a == "end")
+    {
+        calculation = false;
+    }
+}
