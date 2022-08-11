5d5cadc9520c11e56adee1d777524a384b3d0d8f
Alexey Skoblikov <happycow32@yandex.ru>
2022-08-11 16:11:27 +0300

    Task 13 redact

--- task13(third_init)/Program.cs
+++ task13(third_init)/Program.cs
@@ -12,12 +12,14 @@ while (true)
     Console.WriteLine("Введите число.");
     int number = Convert.ToInt32(Console.ReadLine());
     string str = Convert.ToString(number);
-    if (str.Length > 2) {
-Console.WriteLine(str[2]);
-        }
-        else {
-            Console.WriteLine("У введенного числа нет третьей цифры.");
-        }
+    if (str.Length > 2)
+    {
+        Console.WriteLine(str[2]);
+    }
+    else
+    {
+        Console.WriteLine("У введенного числа нет третьей цифры.");
+    }
     Console.WriteLine("Для завершения введите end, для продолжения - любой символ");
     string a = Console.ReadLine();
     if (a == "end")
